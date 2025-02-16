4e1e300a268c12747d483a7bb16504187a199898
Joseph <poedude5229@gmail.com>
2025-02-15 21:56:07 -0500

    start 1

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,14 @@
 import { useState } from "react";
 
+const audio = document.getElementById("audio");
+
 function App() {
-  const [count, setCount] = useState(0);
+  const [modeSelect, setModeSelect] = useState(0);
 
   return (
-    <>
-      
-    </>
+    <div className="player">
+      <input type="file" accept="audio/mp3" />
+    </div>
   );
 }
 
