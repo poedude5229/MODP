8e46add0e074d5467a908915ec9c405687ce7f5e
Joseph <poedude5229@gmail.com>
2025-02-15 22:02:00 -0500

    start 2

--- src/App.jsx
+++ src/App.jsx
@@ -7,7 +7,19 @@ function App() {
 
   return (
     <div className="player">
-      <input type="file" accept="audio/mp3" />
+      <input type="file" id="fileInput" accept="audio/mp3" />
+      <audio src="https://bandfishbucket.s3.us-east-1.amazonaws.com/Sublime+X+Stick+Figure++Feel+Like+That+feat+Bradley+Nowell.mp3"></audio>
+      <br />
+      <button
+        onClick={() => {
+          playAudio();
+        }}
+      >Play</button>
+      <button
+        onClick={() => {
+          pauseAudio();
+        }}
+      >Pause</button>
     </div>
   );
 }
